192b51f71f10003f4d46b2344dbecf9ace8c42f9
Fernando Uriel Perez Meneses <122332137+Menesesitas@users.noreply.github.com>
2023-01-10 22:13:40 -0600

    Add files via upload

---
+++ Actividad_1_Lab/Actividad_1_Lab/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Actividad_1_Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 20F);
+            this.label1.Location = new System.Drawing.Point(175, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(448, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre: Fernando Uriel Perez Meneses";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 20F);
+            this.label2.Location = new System.Drawing.Point(350, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID: 170153";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 20F);
+            this.label3.Location = new System.Drawing.Point(250, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(321, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Laboratorio de Videojuegos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
