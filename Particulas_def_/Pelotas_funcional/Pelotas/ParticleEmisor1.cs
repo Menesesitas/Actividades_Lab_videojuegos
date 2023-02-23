59bc7ba4a43273660e62e449d3863dd26cfaba1a
Fernando Uriel Perez Meneses <122332137+Menesesitas@users.noreply.github.com>
2023-02-22 23:18:16 -0600

    Add files via upload

---
+++ Particulas_def_/Pelotas_funcional/Pelotas/ParticleEmisor1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Pelotas
+{
+    public class ParticleEmisor1
+    {
+        private Random rand;
+        public List<Pelota> particulas;
+
+        private Size space;
+        private int max;
+
+        private float emision;
+        private float timer;
+
+        public float x;
+        public float y;
+        public ParticleEmisor1(Random rand, Size space, int max, float emision)
+        {
+            this.rand = rand;
+            this.particulas = new List<Pelota>();
+            this.space = space;
+            this.max = max;
+            this.emision = emision;
+            this.timer = 0;
+            this.x = 70;
+            this.y = 0;
+        }
+
+
+        public void Update(float deltaTime)
+        {
+            if (particulas.Count < max)
+            {
+                timer += deltaTime;
+
+                if (timer >= 0 / emision)
+                {
+                    Pelota newParticle = new Pelota(rand, space, particulas.Count);
+                    timer = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Particulas_def_/Pelotas_funcional/Pelotas/Pelotas.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using Pelotas.Properties;
+using System.Drawing.Imaging;
+
+namespace Pelotas
+{
+    public partial class Pelotas : Form
+    {
+        static List<Pelota> balls;
+        static Bitmap bmp;
+        static Graphics g;
+        static Random rand = new Random();
+        static float deltaTime;
+        static float Timelap;
+
+        
+        public int alpha = 255;
+
+        ParticleEmisor1 emitter = new ParticleEmisor1(rand, new Size(800, 600), 100, 100);
+
+        public Pelotas()
+        {
+            InitializeComponent();
+        }
+
+        private void Init()
+        {
+            if (PCT_CANVAS.Width == 0)
+                return;
+
+            balls       = new List<Pelota>();
+            bmp         = new Bitmap(PCT_CANVAS.Width, PCT_CANVAS.Height);
+            g           = Graphics.FromImage(bmp);
+            deltaTime   = 1;
+            PCT_CANVAS.Image = bmp;
+
+            for (int b = 0; b < 500; b++)
+                balls.Add(new Pelota (rand, PCT_CANVAS.Size, b));
+        }
+
+        private void Pelotas_Load(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void Pelotas_SizeChanged(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void TIMER_Tick(object sender, EventArgs e)
+        {
+            g.Clear(Color.Black);
+            emitter.Update(deltaTime);
+
+            for (int i = balls.Count - 1; i >= 0; i--)
+            {
+                Pelota P;
+                P = balls[i];
+                balls[i].Update(deltaTime, balls);
+            }
+            /*Parallel.For(0, balls.Count, b =>//ACTUALIZAMOS EN PARALELO
+            {
+                Pelota P;
+                balls[b].Update(deltaTime, balls);
+                P = balls[b];               
+            });*/
+
+            Pelota p;
+            for (int b = 0; b < balls.Count; b++)//PINTAMOS EN SECUENCIA
+            {
+                p = balls[b];
+                g.FillEllipse(new SolidBrush(p.c), p.x - p.radio, p.y - p.radio, p.radio * 2, p.radio * 2);
+
+                Color color = Color.FromArgb(alpha, 255, 255, 255);
+
+                alpha = 255;
+                alpha -= 5;
+                color = Color.FromArgb(alpha, 255, 255, 255);
+            }
+
+            PCT_CANVAS.Invalidate();
+            deltaTime += .1f;
+            Timelap += deltaTime;
+        }
+    }
+}
